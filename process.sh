d173db6801a3433c1b4ccbad7193b6d197541b9b
Oscar Koeroo <okoeroo@gmail.com>
2015-06-07 20:11:31 +0200

    Change framecount extraction, now with a Bash dependant regex.

--- process.sh
+++ process.sh
@@ -12,7 +12,13 @@ mute() { ### UNUSED
 
 getframe() {
     FILE="$1"
-    FRAMECOUNT=$(ffmpeg -i "${FILE}" -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep 'frame=' |  tr -s ' ' | cut -f 2 -d ' ')
+    #FRAMECOUNT=$(ffmpeg -i "${FILE}" -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep 'frame=' |  tr -s ' ' | cut -f 2 -d ' ')
+
+    # Get the frame count through a regex
+    string=$(ffmpeg -i "${FILE}" -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep 'frame=');
+    [[ $string =~ [^0-9]*([0-9]+)[^0-9]+ ]];
+    FRAMECOUNT="${BASH_REMATCH[1]}"
+
     echo $FRAMECOUNT
 }
 
@@ -47,6 +53,7 @@ fademagic() {
     elif [ $FADE_IN_FRAMECOUNT -ne 0 ] && [ $FADE_OUT_FRAMECOUNT -eq 0 ]; then
         FILTER="-filter:v fade=in:0:$FADEFRAMECOUNT"
     else
+        echo "Frame count: $FADEFRAMECOUNT"
         FILTER="-filter:v fade=in:0:$FADEFRAMECOUNT,fade=out:$(($FRAMECOUNT-$FADEFRAMECOUNT)):$FADEFRAMECOUNT"
     fi
 
@@ -56,6 +63,10 @@ fademagic() {
 
 
 concatclips() {
+    FINAL="$1"
+    shift
+
+
     TMPFILE="/tmp/movie_magic.tmp.list.tmp"
     rm "${TMPFILE}"
     CNT=0
@@ -83,7 +94,7 @@ concatclips() {
     done
 
     # Concat clips
-    FINAL="final.mp4"
+#    FINAL="final.mp4"
     ffmpeg -y -f concat -i "$TMPFILE" -acodec copy -c copy "${FINAL}"
 
     # CLEAN UP
@@ -96,47 +107,10 @@ concatclips() {
     echo "Final file: ${FINAL}"
 }
 
-#concatclips "materials/CISO_infinity_logo_1280x720.noaudio.mp4" "De Kraaien - 1&2-f_iM-CusiZU.mp4" "materials/CISO_infinity_logo_1280x720.noaudio.mp4"
-concatclips "materials/CISO_infinity_logo_1280x720.noaudio.mp4" "De Kraaien - 1&2-f_iM-CusiZU.mp4"
+# describe videos here
+concatclips "final4.mp4" \
+    "materials/CISO title clips/NCSC One 2015 - Jaya Baloo - Crypto is dead, long live crypto.mp4" \
+    "raw/NCSC One - Jaya Baloo - Crypto is dead, long live crypto.mp4" \
+    "materials/CISO_infinity_logo_1280x720.noaudio.mp4"
 
 
-exit 0
-
-fademagic "materials/CISO_infinity_logo_1280x720.noaudio.mp4" "tmp.mp4" 0 $FADEFRAMECOUNT
-echo "file tmp.mp4" >> tmp.list.tmp
-fademagic "materials/CISO_infinity_logo_1280x720.noaudio.mp4" "tmp2.mp4" $FADEFRAMECOUNT $FADEFRAMECOUNT
-echo "file tmp2.mp4" >> tmp.list.tmp
-
-ffmpeg -f concat -i tmp.list.tmp -c copy final.mp4
-
-
-
-
-#ffmpeg -i input.mp4 -filter:v 'fade=in:0:50,fade=out:21000:50' -c:v libx264 -crf 22 -preset veryfast -c:a copy output.mp4
-
-
-exit 0
-
-ffmpeg \
-    -i "materials/CISO_infinity_logo_1280x720.noaudio.mp4" \
-    -i "De Kraaien - 1&2-f_iM-CusiZU.mp4" \
-    -f lavfi -i color=black -filter_complex \
-"[0:v]format=pix_fmts=yuva420p,fade=t=out:st=3.5:d=0.4:alpha=1,setpts=PTS-STARTPTS[va0];\
-[1:v]format=pix_fmts=yuva420p,fade=t=in:st=0:d=1:alpha=1,setpts=PTS-STARTPTS+4/TB[va1];\
-[2:v]scale=1280x720,trim=duration=9[over],amerge=inputs=2;\
-[over][va0]overlay[over1];\
-[over1][va1]overlay=format=yuv420[outv];" \
-    -aspect "16/9"    -vcodec libx264 -map [outv] out.mp4
-
-
-
-exit 0
-
-# Fade in from black from frame 0 to frame 50
-# ffmpeg -i input.mp4 -filter:v 'fade=in:0:50' -c:v libx264 -crf 22 -preset veryfast -c:a copy output.mp4
-
-# Fade out to black from frame 21000 to frame 21050
-# ffmpeg -i input.mp4 -filter:v 'fade=out:21000:50' -c:v libx264 -crf 22 -preset veryfast -c:a copy output.mp4
-
-# Fade in and out combined
-# ffmpeg -i input.mp4 -filter:v 'fade=in:0:50,fade=out:21000:50' -c:v libx264 -crf 22 -preset veryfast -c:a copy output.mp4
